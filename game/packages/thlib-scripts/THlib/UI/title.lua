685a8d0ae8eb6d7c60df58a1afc8712451b36da6
璀境石 <Kuanlan@outlook.com>
2023-08-19 15:44:32 +0800

    神秘存档机制

--- game/packages/thlib-scripts/THlib/UI/title.lua
+++ game/packages/thlib-scripts/THlib/UI/title.lua
@@ -11,6 +11,15 @@ function stage_init:render()
     ui.DrawMenuBG()
 end
 
+local stage_quit = stage.New("stage.quit", false, true)
+function stage_quit:init()
+    -- 添加一个单独的关卡，退出游戏时会切换到这个关卡
+    -- 这是为了触发切换关卡时自动存档
+    stage.QuitGame()
+end
+function stage_quit:render()
+end
+
 MusicRecord("menu", 'THlib/music/luastg 0.08.540 - 1.27.800.ogg', 87.8, 79.26)
 MusicRecord("spellcard", 'THlib/music/spellcard.ogg', 75, 0xc36e80 / 44100 / 4)
 
@@ -35,8 +44,12 @@ function stage_menu:init()
         end)
         task.New(stage_menu, function()
             menu.FlyOut(menu_title, 'right')
-            task.Wait(60)
-            stage.QuitGame()
+            task.Wait(30)
+            New(mask_fader, 'close')
+            task.Wait(29)
+            self.no_bg = true
+            task.Wait(1)
+            stage.Set("stage.quit")
         end)
     end
     --
@@ -260,5 +273,7 @@ function stage_menu:init()
     menu_list = { menu_title, menu_player_select, menu_difficulty_select, menu_replay_loader, menu_replay_saver, menu_items, menu_sc_pr, menu_network, menu_player_select2, menu_player_select1, menu_playercount }--设置菜单对象表
 end
 function stage_menu:render()
-    ui.DrawMenuBG()
+    if not self.no_bg then
+        ui.DrawMenuBG()
+    end
 end
