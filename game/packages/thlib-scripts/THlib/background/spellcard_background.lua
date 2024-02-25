fcf55ff3861d5cd688baf5e47a2f207ade843257
TengoDango <1353878448@qq.com>
2024-02-25 15:26:28 +0800

    fix: 符卡背景 tile 为 true 时, rot, hscale 等属性不起作用

--- game/packages/thlib-scripts/THlib/background/spellcard_background.lua
+++ game/packages/thlib-scripts/THlib/background/spellcard_background.lua
@@ -54,6 +54,44 @@ function _spellcard_background:frame()
     end
 end
 
+local function transform(x, y, x0, y0, base, width, height)
+    x, y = x - x0, y - y0
+    return
+        (x * base[1][1] + y * base[1][2]) / width,
+        (x * base[2][1] + y * base[2][2]) / height
+end
+
+local function tileRender(self)
+    local rot = self.rot
+    local width, height = GetImageSize(self.img)
+    width, height = width * self.hscale, height * self.vscale
+    local base = {
+        { cos(rot),  sin(rot) },
+        { -sin(rot), cos(rot) },
+    }
+
+    local w = lstg.world
+    local p1x, p1y = transform(w.l, w.t, self.x, self.y, base, width, height)
+    local p2x, p2y = transform(w.r, w.t, self.x, self.y, base, width, height)
+    local p3x, p3y = transform(w.r, w.b, self.x, self.y, base, width, height)
+    local p4x, p4y = transform(w.l, w.b, self.x, self.y, base, width, height)
+
+    local left = min(p1x, p2x, p3x, p4x)
+    local right = max(p1x, p2x, p3x, p4x)
+    local bottom = min(p1y, p2y, p3y, p4y)
+    local top = max(p1y, p2y, p3y, p4y)
+
+    for i = math.floor(left), math.ceil(right) do
+        for j = math.floor(bottom), math.ceil(top) do
+            Render(self.img,
+                self.x + i * width * base[1][1] + j * height * base[2][1],
+                self.y + i * width * base[1][2] + j * height * base[2][2],
+                self.rot, self.hscale, self.vscale
+            )
+        end
+    end
+end
+
 function _spellcard_background:render()
     SetViewMode 'world'
     if self.alpha > 0 then
@@ -65,14 +103,15 @@ function _spellcard_background:render()
             local l = self.layers[i]
             l._cur_alpha = self.alpha
             SetImageState(l.img, l.blend, Color(l.a * self.alpha, l.r, l.g, l.b))
-            local world = lstg.world
+            -- local world = lstg.world
             if l.tile then
-                local w, h = GetTextureSize(l.img)
-                for i = -int((world.r + 16 + l.x) / w + 0.5), int((world.r + 16 - l.x) / w + 0.5) do
-                    for j = -int((world.t + 16 + l.y) / h + 0.5), int((world.t + 16 - l.y) / h + 0.5) do
-                        Render(l.img, l.x + i * w, l.y + j * h)
-                    end
-                end
+                tileRender(l)
+                -- local w, h = GetTextureSize(l.img)
+                -- for i = -int((world.r + 16 + l.x) / w + 0.5), int((world.r + 16 - l.x) / w + 0.5) do
+                --     for j = -int((world.t + 16 + l.y) / h + 0.5), int((world.t + 16 - l.y) / h + 0.5) do
+                --         Render(l.img, l.x + i * w, l.y + j * h)
+                --     end
+                -- end
             else
                 Render(l.img, l.x, l.y, l.rot, l.hscale, l.vscale)
             end
