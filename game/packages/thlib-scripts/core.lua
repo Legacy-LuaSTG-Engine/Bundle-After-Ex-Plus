df2f01dbc07e14463fd72616c23008a712dfbb01
璀境石 <Kuanlan@outlook.com>
2024-03-17 15:01:24 +0800

    修缮task系统，移除taskself属性匹配，标记task.Until废弃，移动task.Move系列函数到单独文件

--- game/packages/thlib-scripts/core.lua
+++ game/packages/thlib-scripts/core.lua
@@ -18,7 +18,8 @@ lstg.DoFile("lib/Lobject.lua")--Luastg的Class、object
 lstg.DoFile("lib/Lresources.lua")--资源的加载函数、资源枚举和判断
 lstg.DoFile("lib/Lscreen.lua")--world、3d、viewmode的参数设置
 lstg.DoFile("lib/Linput.lua")--按键状态更新
-lstg.DoFile("lib/Ltask.lua")--task
+---@diagnostic disable-next-line: lowercase-global
+task = require("lib.Ltaskmove")--task
 lstg.DoFile("lib/Lstage.lua")--stage关卡系统
 lstg.DoFile("lib/Ltext.lua")--文字渲染
 require("foundation.legacy.scoredata")
