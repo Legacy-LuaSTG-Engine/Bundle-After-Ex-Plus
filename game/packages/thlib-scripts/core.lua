1a0673ca861b560d1b44a586c29a23dbbb2f5c23
OLC <utsumabo@ritsukage.com>
2024-10-13 19:18:50 +0800

    Revert "修正 eventType 的注释显示样式"
    This reverts commit 89930ea55d7df1da748dfeb3b60a1d2003c38fae.

--- game/packages/thlib-scripts/core.lua
+++ game/packages/thlib-scripts/core.lua
@@ -10,29 +10,29 @@ local lstg = lstg -- optimization for local access
 ----------------------------------------
 ---各个模块
 
-lstg.DoFile('"gconfig.lua"')--全局配置信息
-lstg.DoFile('"lib/Llog.lua"')--简单的log系统
-lstg.DoFile('"lib/Lglobal.lua"')--用户全局变量
-lstg.DoFile('"lib/Lmath.lua"')--数学常量、数学函数、随机数系统
-lstg.DoFile('"plus/plus.lua"')--CHU神的plus库，replay系统、plusClass、NativeAPI
-lstg.DoFile('"lib/Lobject.lua"')--Luastg的Class、object
-lstg.DoFile('"lib/Lresources.lua"')--资源的加载函数、资源枚举和判断
-lstg.DoFile('"lib/Lscreen.lua"')--world、3d、viewmode的参数设置
-lstg.DoFile('"lib/Linput.lua"')--按键状态更新
+lstg.DoFile("gconfig.lua")--全局配置信息
+lstg.DoFile("lib/Llog.lua")--简单的log系统
+lstg.DoFile("lib/Lglobal.lua")--用户全局变量
+lstg.DoFile("lib/Lmath.lua")--数学常量、数学函数、随机数系统
+lstg.DoFile("plus/plus.lua")--CHU神的plus库，replay系统、plusClass、NativeAPI
+lstg.DoFile("lib/Lobject.lua")--Luastg的Class、object
+lstg.DoFile("lib/Lresources.lua")--资源的加载函数、资源枚举和判断
+lstg.DoFile("lib/Lscreen.lua")--world、3d、viewmode的参数设置
+lstg.DoFile("lib/Linput.lua")--按键状态更新
 ---@diagnostic disable-next-line: lowercase-global
-task = require('"lib.Ltaskmove"')--task
-lstg.DoFile('"lib/Lstage.lua"')--stage关卡系统
-lstg.DoFile('"lib/Ltext.lua"')--文字渲染
-require('"foundation.legacy.scoredata"')
-lstg.DoFile('"lib/Lplugin.lua"')--用户插件
-require('"lib.debug.AllView"')
-require('"foundation.MainLoop"')
-local createEventDispatcher = require('"foundation.EventDispatcher"')
+task = require("lib.Ltaskmove")--task
+lstg.DoFile("lib/Lstage.lua")--stage关卡系统
+lstg.DoFile("lib/Ltext.lua")--文字渲染
+require("foundation.legacy.scoredata")
+lstg.DoFile("lib/Lplugin.lua")--用户插件
+require("lib.debug.AllView")
+require("foundation.MainLoop")
+local createEventDispatcher = require("foundation.EventDispatcher")
 eventListener = createEventDispatcher -- for compatibility
 
 --------------------------------------------------------------------------------
 
----@alias lstg.GameStateEventGroup '"GameState.AfterGetInput"' | '"GameState.BeforeGameStageChange"' | '"GameState.AfterGameStageChange"' | '"GameState.BeforeGameStageUpdate"' | '"GameState.AfterGameStageUpdate"' | '"GameState.BeforeObjFrame"' | '"GameState.AfterObjFrame"' | '"GameState.BeforeBoundCheck"' | '"GameState.AfterBoundCheck"' | '"GameState.BeforeCollisionCheck"' | '"GameState.AfterCollisionCheck"' | '"GameState.BeforeRender"' | '"GameState.AfterRender"' | '"GameState.BeforeStageRender"' | '"GameState.AfterStageRender"' | '"GameState.BeforeObjRender"' | '"GameState.AfterObjRender"'
+---@alias lstg.GameStateEventGroup "GameState.AfterGetInput" | "GameState.BeforeGameStageChange" | "GameState.AfterGameStageChange" | "GameState.BeforeGameStageUpdate" | "GameState.AfterGameStageUpdate" | "GameState.BeforeObjFrame" | "GameState.AfterObjFrame" | "GameState.BeforeBoundCheck" | "GameState.AfterBoundCheck" | "GameState.BeforeCollisionCheck" | "GameState.AfterCollisionCheck" | "GameState.BeforeRender" | "GameState.AfterRender" | "GameState.BeforeStageRender" | "GameState.AfterStageRender" | "GameState.BeforeObjRender" | "GameState.AfterObjRender"
 
 ---@class lstg.GlobalEventDispatcher : foundation.EventDispatcher
 ---@field FindEvent fun(self: lstg.GlobalEventDispatcher, eventType: lstg.GameStateEventGroup, name: string): foundation.EventDispatcher.Event
@@ -44,7 +44,7 @@ lstg.globalEventDispatcher = gameEventDispatcher
 
 --------------------------------------------------------------------------------
 
-local SceneManager = require('"foundation.SceneManager"')
+local SceneManager = require("foundation.SceneManager")
 
 --------------------------------------------------------------------------------
 --- 默认的主场景
@@ -53,32 +53,32 @@ local SceneManager = require('"foundation.SceneManager"')
 --- 逻辑帧更新，不和 FrameFunc 一一对应
 function DoFrame()
     -- 设置标题
-    --lstg.SetTitle(string.format('"%s | %.2f FPS | %d OBJ | %s"', setting.mod, lstg.GetFPS(), lstg.GetnObj(), gconfig.window_title))
-    lstg.SetTitle(string.format('"%s"', gconfig.window_title)) -- 启动器阶段不用显示那么多信息
+    --lstg.SetTitle(string.format("%s | %.2f FPS | %d OBJ | %s", setting.mod, lstg.GetFPS(), lstg.GetnObj(), gconfig.window_title))
+    lstg.SetTitle(string.format("%s", gconfig.window_title)) -- 启动器阶段不用显示那么多信息
     -- 获取输入
     GetInput()
-    gameEventDispatcher:DispatchEvent('"GameState.AfterGetInput"')
+    gameEventDispatcher:DispatchEvent("GameState.AfterGetInput")
     -- 切关处理
     if stage.NextStageExist() then
-        gameEventDispatcher:DispatchEvent('"GameState.BeforeGameStageChange"')
+        gameEventDispatcher:DispatchEvent("GameState.BeforeGameStageChange")
         stage.Change()
-        gameEventDispatcher:DispatchEvent('"GameState.AfterGameStageChange"')
+        gameEventDispatcher:DispatchEvent("GameState.AfterGameStageChange")
     end
     -- 上一帧的处理
     lstg.AfterFrame(2) -- TODO: remove (2)
     -- 关卡更新
-    gameEventDispatcher:DispatchEvent('"GameState.BeforeGameStageUpdate"')
+    gameEventDispatcher:DispatchEvent("GameState.BeforeGameStageUpdate")
     stage.Update()
-    gameEventDispatcher:DispatchEvent('"GameState.AfterGameStageUpdate"')
+    gameEventDispatcher:DispatchEvent("GameState.AfterGameStageUpdate")
     -- 游戏对象更新
-    gameEventDispatcher:DispatchEvent('"GameState.BeforeObjFrame"')
+    gameEventDispatcher:DispatchEvent("GameState.BeforeObjFrame")
     lstg.ObjFrame(2) -- TODO: remove (2)
-    gameEventDispatcher:DispatchEvent('"GameState.AfterObjFrame"')
+    gameEventDispatcher:DispatchEvent("GameState.AfterObjFrame")
     -- 碰撞检测
-    gameEventDispatcher:DispatchEvent('"GameState.BeforeBoundCheck"')
+    gameEventDispatcher:DispatchEvent("GameState.BeforeBoundCheck")
     lstg.BoundCheck()
-    gameEventDispatcher:DispatchEvent('"GameState.AfterBoundCheck"')
-    gameEventDispatcher:DispatchEvent('"GameState.BeforeCollisionCheck"')
+    gameEventDispatcher:DispatchEvent("GameState.AfterBoundCheck")
+    gameEventDispatcher:DispatchEvent("GameState.BeforeCollisionCheck")
     -- TODO: 等 API 文档更新后，去除下一行的禁用警告
     ---@diagnostic disable-next-line: param-type-mismatch, missing-parameter
     lstg.CollisionCheck({
@@ -97,19 +97,19 @@ function DoFrame()
         -- 用于检查与自机碰撞 (by OLC)
         { GROUP_CPLAYER, GROUP_PLAYER },
     });
-    gameEventDispatcher:DispatchEvent('"GameState.AfterCollisionCheck"')
+    gameEventDispatcher:DispatchEvent("GameState.AfterCollisionCheck")
 end
 
 function BeforeRender()
-    gameEventDispatcher:DispatchEvent('"GameState.BeforeRender"')
+    gameEventDispatcher:DispatchEvent("GameState.BeforeRender")
 end
 
 function AfterRender()
-    gameEventDispatcher:DispatchEvent('"GameState.AfterRender"')
+    gameEventDispatcher:DispatchEvent("GameState.AfterRender")
 end
 
 ---@class game.DefaultScene : foundation.Scene
-local DefaultScene = SceneManager.add('"DefaultScene"')
+local DefaultScene = SceneManager.add("DefaultScene")
 
 DefaultScene.initialized = false
 
@@ -121,7 +121,7 @@ function DefaultScene:onCreate()
     -- 加载mod包
     if setting.mod ~= 'launcher' then
         Include 'root.lua'
-        lstg.plugin.DispatchEvent('"afterMod"')
+        lstg.plugin.DispatchEvent("afterMod")
     else
         Include 'launcher.lua'
     end
@@ -129,11 +129,11 @@ function DefaultScene:onCreate()
     lstg.RegisterAllGameObjectClass() -- 对所有class的回调函数进行整理，给底层调用
     InitScoreData() -- 装载玩家存档
 
-    SetViewMode('"world"')
+    SetViewMode("world")
     --if stage.next_stage == nil then
     --    error('Entrance stage not set.')
     --end
-    SetResourceStatus('"stage"')
+    SetResourceStatus("stage")
 
     DefaultScene.initialized = true
 end
@@ -147,13 +147,13 @@ end
 
 function DefaultScene:onRender()
     BeforeRender()
-    gameEventDispatcher:DispatchEvent('"GameState.BeforeStageRender"')
+    gameEventDispatcher:DispatchEvent("GameState.BeforeStageRender")
     stage.current_stage:render()
-    gameEventDispatcher:DispatchEvent('"GameState.AfterStageRender"')
-    gameEventDispatcher:DispatchEvent('"GameState.BeforeObjRender"')
+    gameEventDispatcher:DispatchEvent("GameState.AfterStageRender")
+    gameEventDispatcher:DispatchEvent("GameState.BeforeObjRender")
     ObjRender()
-    gameEventDispatcher:DispatchEvent('"GameState.AfterObjRender"')
+    gameEventDispatcher:DispatchEvent("GameState.AfterObjRender")
     AfterRender()
 end
 
-SceneManager.setNext('"DefaultScene"')
+SceneManager.setNext("DefaultScene")
