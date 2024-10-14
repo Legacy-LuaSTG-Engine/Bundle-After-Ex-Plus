5e0eac1cfbd95bea18fc951f92a3e08d71e203db
璀境石 <git@chuikingshek.com>
2024-10-14 18:00:42 +0800

    test alpha mask

--- environment/shader/main.lua
+++ environment/shader/main.lua
@@ -1,6 +1,8 @@
 lstg.FileManager.AddSearchPath("../../game/packages/thlib-scripts/")
 lstg.FileManager.AddSearchPath("../../game/packages/thlib-resources/")
 
+local post_effect = require("lib.posteffect")
+
 local window = {
     width = 640,
     height = 480,
@@ -15,37 +17,49 @@ function window:applyCameraSetting()
     lstg.SetZBufferEnable(0)
 end
 
-local post_effect = require("lib.posteffect")
+local function loadSprite(name, path, mipmap)
+    lstg.LoadTexture(name, path, mipmap)
+    local width, height = lstg.GetTextureSize(name)
+    lstg.LoadImage(name, name, 0, 0, width, height)
+end
+
+local timer = -1
 
 function GameInit()
     lstg.ChangeVideoMode(window.width, window.height, true, false)
-    lstg.CreateRenderTarget("canvas1")
-    lstg.CreateRenderTarget("mask1")
+    lstg.CreateRenderTarget("rt:canvas1")
+    lstg.CreateRenderTarget("rt:mask1")
+    loadSprite("canvas1", "canvas1.png")
+    loadSprite("mask1", "mask1.png")
 end
 
 function FrameFunc()
+    timer = timer + 1
 	return false
 end
 
 function RenderFunc()
     lstg.BeginScene()
 
-    lstg.PushRenderTarget("canvas1")
+    lstg.PushRenderTarget("rt:canvas1")
     do
         window:applyCameraSetting()
         lstg.RenderClear(lstg.Color(255, 0, 0, 0))
+        lstg.Render("canvas1", window.width / 2, window.height / 2)
     end
-    lstg.PopRenderTarget() -- "canvas1"
+    lstg.PopRenderTarget() -- "rt:canvas1"
 
-    lstg.PushRenderTarget("mask1")
+    lstg.PushRenderTarget("rt:mask1")
     do
         window:applyCameraSetting()
         lstg.RenderClear(lstg.Color(255, 0, 0, 0))
+        local dy = (0.5 * window.height / 2) * lstg.sin(timer)
+        lstg.Render("mask1", window.width / 2, window.height / 2 + dy)
     end
-    lstg.PopRenderTarget() -- "mask1"
+    lstg.PopRenderTarget() -- "rt:mask1"
 
     window:applyCameraSetting()
-    post_effect.drawMaskEffect("canvas1", "mask1")
+    post_effect.drawMaskEffect("rt:canvas1", "rt:mask1")
 
     lstg.EndScene()
 end
