054d0cfc63ed48392d45132e3cd848560df45b31
璀境石 <git@chuikingshek.com>
2025-02-03 12:26:31 +0800

    rm thlib-legacy-bullet-definitions

--- game/plugins/thlib-legacy-bullet-definitions/index.lua
+++
@@ -1,5 +0,0 @@
-LoadTexture('bullet1', 'THlib/bullet/bullet1.png', true) -- TODO: 杀杀杀
-
-lstg.plugin.RegisterEvent("afterTHlib", "thlib-legacy-bullet-definitions", 0, function()
-    lstg.DoFile("THlib/bullet/legacy_bullet.lua")
-end)
