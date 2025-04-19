da3fe4b266ec3b7334dcd2644c34126295bd42da
璀境石 <git@chuikingshek.com>
2025-04-19 23:38:37 +0800

    stay on create

--- game/packages/thlib-scripts/stg/bullet/EnemyBullet.lua
+++ game/packages/thlib-scripts/stg/bullet/EnemyBullet.lua
@@ -89,7 +89,14 @@ function EnemyBullet:kill()
 end
 
 function EnemyBullet:frame()
-    task.Do(self)
+    if self.stay and self.timer < self.fade_in_frames then
+        -- magic behavior
+        self.x = self.x - self.vx
+        self.y = self.y - self.vy
+        self.rot = self.rot - self.omiga
+    else
+        task.Do(self)
+    end
 end
 
 function EnemyBullet:render()
