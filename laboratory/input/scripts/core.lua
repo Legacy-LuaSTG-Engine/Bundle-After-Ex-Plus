28c5b3df68677ce10819fd0c1f721b414b4fd520
璀境石 <git@chuikingshek.com>
2025-08-24 06:46:16 +0800

    laboratory -- input: replay

--- laboratory/input/scripts/core.lua
+++ laboratory/input/scripts/core.lua
@@ -3,19 +3,15 @@ local Keyboard = lstg.Input.Keyboard
 local InputSystem = require("foundation.InputSystem")
 local Viewport = require("laboratory.input.Viewport")
 
-local action_set = InputSystem.addActionSet("menu")
+local action_set = InputSystem.addActionSet("player")
 action_set:addBooleanAction("left")
     :addKeyboardKeyBinding(Keyboard.Left)
-    :addKeyboardKeyBinding(Keyboard.A)
 action_set:addBooleanAction("right")
     :addKeyboardKeyBinding(Keyboard.Right)
-    :addKeyboardKeyBinding(Keyboard.D)
 action_set:addBooleanAction("up")
     :addKeyboardKeyBinding(Keyboard.Up)
-    :addKeyboardKeyBinding(Keyboard.W)
 action_set:addBooleanAction("down")
     :addKeyboardKeyBinding(Keyboard.Down)
-    :addKeyboardKeyBinding(Keyboard.S)
 action_set:addBooleanAction("slow")
     :addKeyboardKeyBinding(Keyboard.LeftShift)
     :addKeyboardKeyBinding(Keyboard.RightShift)
@@ -23,6 +19,12 @@ action_set:addBooleanAction("size-smaller")
     :addKeyboardKeyBinding(Keyboard.Minus)
 action_set:addBooleanAction("size-larger")
     :addKeyboardKeyBinding(Keyboard.Plus)
+action_set:addVector2Action("fps-move")
+    :addKeyboardKeyBinding(Keyboard.D, Keyboard.A, Keyboard.W, Keyboard.S)
+
+local action_set_menu = InputSystem.addActionSet("menu")
+action_set_menu:addBooleanAction("record")
+    :addKeyboardKeyBinding(Keyboard.R)
 
 local x = 0
 local y = 0
@@ -36,23 +38,65 @@ function GameInit()
 
     lstg.CreateRenderTarget("rt:white", 16, 16, false)
     lstg.LoadImage("white", "rt:white", 0, 0, 16, 16)
+    lstg.LoadTTF("sans", "assets/font/SourceHanSansCN-Bold.otf", 0, 32)
 end
 
 function GameExit()
 end
 
+local begin_x, begin_y = 0, 0
+local end_x, end_y = 0, 0
+local play_mode = "normal"
+local records = {}
+local record_index = 0
+local serde_names = { "player" }
+
 function FrameFunc()
     InputSystem.update()
+    if play_mode == "record" then
+        if record_index > 600 then
+            end_x = x
+            end_y = y
+            x = begin_x
+            y = begin_y
+            play_mode = "playback"
+            record_index = 1
+        end
+    elseif play_mode == "playback" then
+        if record_index > 600 then
+            play_mode = "normal"
+            record_index = 0
+            assert(end_x == x)
+            assert(end_y == y)
+        end
+    else
+        if InputSystem.isBooleanActionActivated("menu:record") then
+            play_mode = "record"
+            record_index = 1
+            begin_x = x
+            begin_y = y
+        end
+    end
+    if play_mode == "record" then
+        local record = InputSystem.serialize(serde_names)
+        records[record_index] = record
+        record_index = record_index + 1
+    elseif play_mode == "playback" then
+        local record = records[record_index]
+        record_index = record_index + 1
+        InputSystem.deserialize(record)
+    end
+
     local dx = 0
-    if InputSystem.getBooleanAction("menu:left") then
+    if InputSystem.getBooleanAction("player:left") then
         dx = dx - 1
-    elseif InputSystem.getBooleanAction("menu:right") then
+    elseif InputSystem.getBooleanAction("player:right") then
         dx = dx + 1
     end
     local dy = 0
-    if InputSystem.getBooleanAction("menu:down") then
+    if InputSystem.getBooleanAction("player:down") then
         dy = dy - 1
-    elseif InputSystem.getBooleanAction("menu:up") then
+    elseif InputSystem.getBooleanAction("player:up") then
         dy = dy + 1
     end
     local v = 1
@@ -60,17 +104,19 @@ function FrameFunc()
         v = 0.70710678118654752440084436210485
     end
     local speed = 10
-    if InputSystem.getBooleanAction("menu:slow") then
+    if InputSystem.getBooleanAction("player:slow") then
         speed = 5
     end
-    x = x + v * speed * dx
-    y = y + v * speed * dy
-    if InputSystem.isBooleanActionActivated("menu:size-smaller", 60, 60) then
+    local dx2, dy2 = InputSystem.getVector2Action("player:fps-move")
+    x = x + v * speed * (dx + dx2)
+    y = y + v * speed * (dy + dy2)
+    if InputSystem.isBooleanActionActivated("player:size-smaller", 60, 60) then
         size = math.max(16, size - 16)
     end
-    if InputSystem.isBooleanActionActivated("menu:size-larger", 120, 60) then
+    if InputSystem.isBooleanActionActivated("player:size-larger", 120, 60) then
         size = size + 16
     end
+
     return false
 end
 
@@ -84,5 +130,7 @@ function RenderFunc()
     end
     Viewport.apply()
     lstg.Render("white", x, y, 0, size / 16)
+    local text = ("%03d -- %s"):format(record_index, play_mode)
+    lstg.RenderTTF("sans", text, 10, 10, Viewport.height - 10, Viewport.height - 10, 0 + 0, lstg.Color(255, 255, 255, 255), 2)
     lstg.EndScene()
 end
