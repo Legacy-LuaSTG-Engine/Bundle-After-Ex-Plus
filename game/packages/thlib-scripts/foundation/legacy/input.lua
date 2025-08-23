1bec26465a601639942130f306b5ab60bded006f
璀境石 <git@chuikingshek.com>
2025-08-24 01:56:31 +0800

    game ActionSet, sync legacy key setting

--- game/packages/thlib-scripts/foundation/legacy/input.lua
+++ game/packages/thlib-scripts/foundation/legacy/input.lua
@@ -38,32 +38,66 @@ menu_action_set:addBooleanAction("cancel")
 menu_action_set:addBooleanAction("special")
     :addKeyboardKeyBinding(Keyboard.C)
 
--- 呼出菜单（一般用于游戏时打开暂停菜单）
+-- 打开菜单
+-- 游戏时/回放时：打开暂停菜单
 
-menu_action_set:addBooleanAction("menu")
+local menu_action_menu = menu_action_set:addBooleanAction("menu")
     :addKeyboardKeyBinding(Keyboard.Escape)
 
 -- 加速、减速
 -- 用户界面：加速或减速页面、滑块（Slider）移动等
 -- 回放模式（Replay）：加速、减速回放
 
-menu_action_set:addBooleanAction("slow-down")
+local menu_action_slow_down = menu_action_set:addBooleanAction("slow-down")
     :addKeyboardKeyBinding(Keyboard.LeftShift)
     :addKeyboardKeyBinding(Keyboard.RightShift)
-menu_action_set:addBooleanAction("speed-up")
+local menu_action_speed_up = menu_action_set:addBooleanAction("speed-up")
     :addKeyboardKeyBinding(Keyboard.LeftControl)
     :addKeyboardKeyBinding(Keyboard.RightControl)
 
 -- 重试/重置
 -- 暂停菜单：快速重开关卡/符卡练习
 
-menu_action_set:addBooleanAction("retry")
+local menu_action_retry = menu_action_set:addBooleanAction("retry")
     :addKeyboardKeyBinding(Keyboard.R)
 
 -- 截图
 
-menu_action_set:addBooleanAction("snapshot")
+local menu_action_snapshot = menu_action_set:addBooleanAction("snapshot")
     :addKeyboardKeyBinding(Keyboard.P)
+
+--------------------------------------------------------------------------------
+--- 游戏动作集（玩家动作集）
+
+local game_action_set = InputSystem.addActionSet("game")
+
+-- 玩家移动
+
+local game_action_left = game_action_set:addBooleanAction("left")
+    :addKeyboardKeyBinding(Keyboard.Left)
+local game_action_right = game_action_set:addBooleanAction("right")
+    :addKeyboardKeyBinding(Keyboard.Right)
+local game_action_down = game_action_set:addBooleanAction("down")
+    :addKeyboardKeyBinding(Keyboard.Down)
+local game_action_up = game_action_set:addBooleanAction("up")
+    :addKeyboardKeyBinding(Keyboard.Up)
+
+-- 玩家移动（矢量）
+
+local game_action_move = game_action_set:addVector2Action("move") -- TODO: 加上绑定
+
+-- 功能键
+
+local game_action_slow = game_action_set:addBooleanAction("slow") -- 用于切换射击模式、移动速度（一般为按下后降低移动速度）
+    :addKeyboardKeyBinding(Keyboard.LeftShift)
+local game_action_shoot = game_action_set:addBooleanAction("shoot") -- 一般用于射击
+    :addKeyboardKeyBinding(Keyboard.Z)
+local game_action_spell = game_action_set:addBooleanAction("spell") -- 一般用于释放符卡（雷/炸弹）
+    :addKeyboardKeyBinding(Keyboard.X)
+local game_action_special = game_action_set:addBooleanAction("special") -- 一般用于释放特殊技能
+    :addKeyboardKeyBinding(Keyboard.C)
+local game_action_skip = game_action_set:addBooleanAction("skip") -- 一般用于跳过对话
+    :addKeyboardKeyBinding(Keyboard.LeftControl)
 
 --------------------------------------------------------------------------------
 --- 兼容性 API
@@ -73,8 +107,70 @@ KEY = require("foundation.legacy.KEY")
 local GAME_ACTION_SET_PREFIX = "game:"
 local MENU_ACTION_SET_PREFIX = "menu:"
 
+local function fillLegacyKeySetting()
+    -- keys
+
+    for _, binding in game_action_left:keyboardBindings() do
+        setting.keys.left = binding.key
+        break
+    end
+    for _, binding in game_action_right:keyboardBindings() do
+        setting.keys.right = binding.key
+        break
+    end
+    for _, binding in game_action_down:keyboardBindings() do
+        setting.keys.down = binding.key
+        break
+    end
+    for _, binding in game_action_up:keyboardBindings() do
+        setting.keys.up = binding.key
+        break
+    end
+
+    for _, binding in game_action_slow:keyboardBindings() do
+        setting.keys.slow = binding.key
+        break
+    end
+    for _, binding in game_action_shoot:keyboardBindings() do
+        setting.keys.shoot = binding.key
+        break
+    end
+    for _, binding in game_action_spell:keyboardBindings() do
+        setting.keys.spell = binding.key
+        break
+    end
+    for _, binding in game_action_special:keyboardBindings() do
+        setting.keys.special = binding.key
+        break
+    end
+
+    -- keysys
+
+    for _, binding in menu_action_menu:keyboardBindings() do
+        setting.keysys.menu = binding.key
+        break
+    end
+    for _, binding in menu_action_slow_down:keyboardBindings() do
+        setting.keysys.repslow = binding.key
+        break
+    end
+    for _, binding in menu_action_speed_up:keyboardBindings() do
+        setting.keysys.repfast = binding.key
+        break
+    end
+    for _, binding in menu_action_retry:keyboardBindings() do
+        setting.keysys.retry = binding.key
+        break
+    end
+    for _, binding in menu_action_snapshot:keyboardBindings() do
+        setting.keysys.snapshot = binding.key
+        break
+    end
+end
+
 function GetInput()
     InputSystem.update()
+    fillLegacyKeySetting()
 end
 
 ---@param key string
@@ -120,3 +216,14 @@ function MenuKeyIsPressed(key)
     key = transformLegacyMenuKey(key)
     return InputSystem.isBooleanActionActivated(MENU_ACTION_SET_PREFIX .. key, 40, 4) -- TODO: 允许修改该默认设置
 end
+
+--------------------------------------------------------------------------------
+--- 导出
+
+---@class legacy.input
+local M = {}
+
+M.fillLegacyKeySetting = fillLegacyKeySetting
+M.transformLegacyMenuKey = transformLegacyMenuKey
+
+return M
